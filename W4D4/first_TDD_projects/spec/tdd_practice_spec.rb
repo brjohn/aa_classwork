b7f6256dc7798db13e9ba84124278e7f69b9764c
Brynn <brynnreginajohnson@gmail.com>
2020-12-10 16:20:11 -0600

    Start first problem

--- W4D4/first_TDD_projects/spec/tdd_practice_spec.rb
+++ W4D4/first_TDD_projects/spec/tdd_practice_spec.rb
@@ -1,1 +1,13 @@
+require 'rspec'
 require 'tdd_practice'
+
+describe Array do
+
+   describe '#my_uniq' do
+        it "takes in an array" do
+           expect {my_uniq([1,2,2,3])}.not_to raise_error 
+        end
+    end
+
+
+end
