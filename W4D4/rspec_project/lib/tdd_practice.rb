aa8ec814d3435f62d12894cb838ee157dc7d91c6
Brynn <brynnreginajohnson@gmail.com>
2020-12-10 17:10:13 -0600

    add to_sum method

--- W4D4/rspec_project/lib/tdd_practice.rb
+++ W4D4/rspec_project/lib/tdd_practice.rb
@@ -7,4 +7,17 @@ class Array
     return hash.keys
   end
 
+  def to_sum
+    return self if self.empty?
+    pairs = []
+    self.each_with_index do |num1, i1|
+      self.each_with_index do |num2, i2|
+        if i2 > i1 && num1 + num2 == 0
+          pairs << [i1, i2]
+        end
+      end
+    end
+    pairs     
+  end
+
 end
--- W4D4/rspec_project/spec/tdd_practice_spec.rb
+++ W4D4/rspec_project/spec/tdd_practice_spec.rb
@@ -12,3 +12,23 @@ describe 'Array#my_uniq' do
    end
 end
 
+describe 'Array#to_sum' do
+   it "works with an empty array" do
+      expect([].to_sum).to eq([]) 
+   end
+  
+   it "returns a 2D array" do
+      expect([1,2,-2,3].to_sum).to eq([[1,2]])
+   end
+
+   it "returns pairs of indices of elements whose sum is 0" do
+      expect([1,2,-2,3,7,-7].to_sum).to eq([[1,2],[4,5]])
+   end
+end
+
+describe 'Array#my_transpose' do
+   it "works with an empty array" do
+      expect([].to_sum).to eq([]) 
+   end
+   
+end
