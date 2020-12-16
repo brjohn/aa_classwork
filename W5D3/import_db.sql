5655f25a123fb792133c641a9ccbdfe65a58e96c
annieyxu <annieyxu@berkeley.edu>
2020-12-16 14:38:28 -0800

    add

--- W5D3/import_db.sql
+++ W5D3/import_db.sql
@@ -14,5 +14,20 @@ CREATE TABLE questions (
 );
 
 CREATE TABLE question_follows (
+    SELECT
+        *
+    FROM
+        users
+    JOIN questions
+        ON users.id = questions.author_id
+)
+
+CREATE TABLE replies (
+    id INTEGER PRIMARY KEY,
+    question_id INTEGER NOT NULL,
+    parent_reply_id INTEGER NOT NULL,
+    FOREIGN KEY (question_id) REFERENCES questions(id),
+    FOREIGN KEY (parent_reply_id) REFERENCES replies(id),
+    FOREIGN kEY () REFERENCES user(id)
     
 )
