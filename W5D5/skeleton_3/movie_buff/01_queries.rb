ae1508ae1c35e746154dee9a6237a84449a6d41e
Brynn <brynnreginajohnson@gmail.com>
2020-12-18 16:32:09 -0600

    Finish 01_queries, through eighties_b_movies

--- W5D5/skeleton_3/movie_buff/01_queries.rb
+++ W5D5/skeleton_3/movie_buff/01_queries.rb
@@ -56,7 +56,8 @@ def directed_by_one_of(them)
   #
   # Find the id and title of all the movies directed by one of 'them'.
 
-  Movie.select(:id, :title).joins(:director)
+  # Movie.select(:id, :title).joins(:director).where('actors.name IN (?)', them)
+  Movie.select(:id, :title).joins(:director).where(actors: {name: them})
 
 end
 
@@ -71,5 +72,7 @@ def movie_names_before_1940
   # improve performace for larger queries.
   #
   # Use pluck to find the title of all movies made before 1940.
+  Movie.where('yr < 1940').pluck(:title)
+
 
 end
