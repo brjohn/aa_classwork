0117f6427e35045d64c3014c153a91aa73ea6a52
Mona Miga <monaleez@love.com>
2020-12-18 14:53:54 -0800

    worked on 02_queries

--- W5D5/skeleton_3/movie_buff/02_queries.rb
+++ W5D5/skeleton_3/movie_buff/02_queries.rb
@@ -2,13 +2,15 @@ def eighties_b_movies
   # List all the movies from 1980-1989 with scores falling between
   # 3 and 5 (inclusive).
   # Show the id, title, year, and score.
-  Movie.select(:id, :title, :yr, :score).where(yr: 1980..1989).where(score: 3..5)
-
+  # Movie.select(:id, :title, :yr, :score).where(yr: 1980..1989).where(score: 3..5)
+  # Movie.select(:id, :title, :yr, :score).where('yr BETWEEN 1980 AND 1989 AND score BETWEEN 3 AND 5')
+  Movie.select(:id, :title, :yr, :score).where(yr: 1980..1989, score: 3..5)
+  
 end
 
 def bad_years
   # List the years in which a movie with a rating above 8 was not released.
-
+  Movie.group(:yr).having.not('score > 8').pluck(:yr)
 end
 
 def cast_list(title)
