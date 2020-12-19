3d0f775b3f241ff1e55fc72439789a03eca61c69
Mona Miga <monaleez@love.com>
2020-12-18 16:03:53 -0800

    worked on 02_queries

--- W5D5/skeleton_3/movie_buff/02_queries.rb
+++ W5D5/skeleton_3/movie_buff/02_queries.rb
@@ -23,15 +23,18 @@ end
 
 def vanity_projects
   # List the title of all movies in which the director also appeared
-  # as the starring actor.
+  # as the starring actor. ord 1
   # Show the movie id and title and director's name.
 
   # Note: Directors appear in the 'actors' table.
+
+  Actor.select('movies.id, movies.title, name').joins(:movies).where('castings.ord = 1 AND director_id = actor_id')
 
 end
 
 def most_supportive
   # Find the two actors with the largest number of non-starring roles.
   # Show each actor's id, name and number of supporting roles.
+  Actor.select(:id, :name, COUNT(*)).joins(:castings).group()
 
 end
