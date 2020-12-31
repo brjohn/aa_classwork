4dc3c609f011b59c6332a402972330dd09d75a3d
Brynn <brynnreginajohnson@gmail.com>
2020-12-31 17:26:16 -0600

    Made Cats index and show pages

--- W6D5/NinetynineCats/db/seeds.rb
+++ W6D5/NinetynineCats/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cat.create([{name: "Jimmy", birth_date: '2000/04/08', color: 'black', sex: 'F', description: 'quite fat'},
+            {name: "Tom", birth_date: '2010/12/05' , color: 'white', sex: 'M', description: 'grumpy'},
+            {name: "Suzie", birth_date: '1980/01/02', color: 'orange', sex: 'F', description: 'advanced in years'}
+])
