4b89ac86d41936f40abe04888b759fb29e4dc39d
Reed Haubenstock <rhaubenstock@users.noreply.github.com>
2020-12-31 16:05:48 -0800

    Adds CatsController#new and view/cats/new.html.erb

--- W6D5/NinetynineCats/app/controllers/cats_controller.rb
+++ W6D5/NinetynineCats/app/controllers/cats_controller.rb
@@ -10,6 +10,12 @@ class CatsController < ApplicationController
     render :show
   end
 
+
+  def new
+    @cat = Cat.new
+    render :new
+  end
+  
   #model - singular
   #controller - plural
   #views - plural
