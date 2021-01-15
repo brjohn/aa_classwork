161d8468565dd9be197e86c94ce5f1b2fdb953e6
Brynn <brynnreginajohnson@gmail.com>
2021-01-15 17:05:29 -0600

    Finish addNumbers

--- w8d5/intro_to_callbacks/addNumbers.js
+++ w8d5/intro_to_callbacks/addNumbers.js
@@ -1,4 +1,4 @@
-const readline = require("readline");
+const readline = require('readline');
 
 const reader = readline.createInterface({
         input: process.stdin,
@@ -6,16 +6,26 @@ const reader = readline.createInterface({
 })
 
 function addNumbers(sum, numsLeft, completionCallback) {
+    let scope = {sum, numsLeft};
     if (numsLeft > 0) {
-        reader.question("Give a number", completionCallback(answer));
+        reader.question("Give a number\n", function(answer) {
+            //completionCallback.call(scope, answer)
+            let value = parseInt(answer);
+            sum += value;
+            completionCallback(sum);
+            addNumbers(sum, (numsLeft-1), completionCallback);
+        });
     } else if (numsLeft === 0) {
-        completionCallback(sum);
+        completionCallback.call(scope, sum);
     }
 }
 
-function completionCallback(answer) {
-    sum = parseInt(answer);
-    sum++;
-    console.log(sum)
-    addNumbers(sum, )
-}
+/*function completionCallback(answer) {
+    let value = parseInt(answer);
+    let sum = this.sum += value ;
+    console.log(sum);
+    addNumbers(sum, this.numsLeft, completionCallback);
+    
+}*/
+
+addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
