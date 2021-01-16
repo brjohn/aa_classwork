79ff33dba6fa59072f69d33b018a160dab45fabc
IsoVoyd <dubnersamuel@gmail.com>
2021-01-15 16:27:32 -0800

    finish myThrottle

--- w8d5/intro_to_callbacks/functionCalling.js
+++ w8d5/intro_to_callbacks/functionCalling.js
@@ -27,7 +27,42 @@ myBoundTurnOn(); // should say "Turning on a lamp"
 */
 
 Function.prototype.myThrottle = function(interval) {
-    let tooSoon = false ;
+    let tooSoon = false;
     
-
+    return () => {
+        if (tooSoon === false) {
+            tooSoon = true;
+            setTimeout(() => tooSoon = false, interval);
+            this();
+        }
+    }
 }
+
+class Neuron {
+  fire() {
+    console.log("Firing!");
+  }
+}
+
+const neuron = new Neuron();
+// When we create a new Neuron,
+// we can call #fire as frequently as we want
+
+// The following code will try to #fire the neuron every 10ms. Try it in the console:
+const interval = setInterval(() => {
+  neuron.fire();
+}, 10);
+
+// You can use clearInterval to stop the firing:
+// clearInterval(interval);
+
+// Using Function#myThrottle, we should be able to throttle
+// the #fire function of our neuron so that it can only fire
+// once every 500ms:
+
+neuron.fire = neuron.fire.myThrottle(500);
+
+// This time, if our Function#myThrottle worked correctly,
+// the Neuron#fire function should only be able to execute
+// every 500ms, even though we're still trying to invoke it
+// every 10ms!
