2310a7879dfea78490e32ed7c27355c3ca87b784
jonathandiaz <jonathan.diaz714@gmail.com>
2021-01-26 20:03:12 -0500

    completed clock functionality, started styling

--- w10d2/Widgets/frontend/clock.jsx
+++ w10d2/Widgets/frontend/clock.jsx
@@ -2,10 +2,47 @@ import React from 'react';
 import Widgets from './widgets';
 
 class Clock extends React.Component {
+    constructor(prop) {
+        super(prop);
+        this.state = {
+            time: new Date()
+        };
+        this.tick = this.tick.bind(this);
+    }
+
+    tick() {
+        this.setState({ time: new Date() })
+    }
+
+    componentDidMount() {
+        //everything in js that isnt a primitive is just a pojo
+        //take this obj, set the id
+        //this is the object(the clock class) so variables declared are available anywhere in the class
+        this.intervalID = setInterval(this.tick, 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.intervalID)
+    }
+
     render(){
+
         console.log("in Clock class");
         return(
-            <h1>Clock</h1>
+            <div>
+                <h1>Clock</h1>
+                <div className="clock">
+                    <p>
+                        <span>Time:</span>
+                        <span>{this.state.time.toLocaleTimeString()}</span>
+                    </p>
+                        <br/>
+                    <p>
+                        <span>Date:</span>
+                        <span>{this.state.time.toDateString()}</span>
+                    </p>
+                </div>
+            </div>
         )
     }
 }
