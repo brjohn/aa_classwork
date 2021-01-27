ef11cf0084a640874d56ab5ad8e581b542023aaa
droid4alex <droid4alex@gmail.com>
2021-01-27 12:04:56 -0800

    Updating render() in game.jsx

--- w10d3/Minesweeper/frontend/game.jsx
+++ w10d3/Minesweeper/frontend/game.jsx
@@ -2,7 +2,23 @@ import React from 'react';
 import * as Minesweeper from '../minesweeper';
 
 class Game extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      board: new Minesweeper.Board()             //object that we continiously update over the course of the game
+    }
+  }
+  updateGame() {
 
+  }
+
+  render (){
+    return (
+      <div>
+        <section>Trying to render a board here</section>
+      </div>
+    )
+  }
 }
 
 export default Game;
