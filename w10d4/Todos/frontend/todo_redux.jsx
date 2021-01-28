47fe043d76b79bd6d39e441b852b1da2f0eec6c1
Brynn <brynnreginajohnson@gmail.com>
2021-01-28 15:51:26 -0600

    Redux store

--- w10d4/Todos/frontend/todo_redux.jsx
+++ w10d4/Todos/frontend/todo_redux.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+const store = configureStore();
 
 document.addEventListener('DOMContentLoaded', () => {
+    window.store = store;
     const reactRoot = document.getElementById('react-root');
     ReactDOM.render(<h1>To-Do's App</h1>, reactRoot);
 })
