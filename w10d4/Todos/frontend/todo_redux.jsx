1c7f63533a79ec254706c040850ce5a7e74e809f
Brynn <brynnreginajohnson@gmail.com>
2021-01-28 13:52:38 -0600

    To-Do's setup

---
+++ w10d4/Todos/frontend/todo_redux.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const reactRoot = document.getElementById('react-root');
+    ReactDOM.render(<h1>To-Do's App</h1>, reactRoot);
+})
