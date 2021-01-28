db9180d81e9fe14093ca0082b1bedbbb38b1bb0c
Edwin Cheng <edwincheng3@gmail.com>
2021-01-28 14:21:09 -0800

    Working on receiving and reducing todos

--- w10d4/Todos/frontend/reducers/todos_reducer.js
+++ w10d4/Todos/frontend/reducers/todos_reducer.js
@@ -1,3 +1,6 @@
+import { RECEIVE_TODOS } from '../actions/todo_actions'
+import { RECEIVE_TODO } from '../actions/todo_actions'
+
 const initialState = {
     1: {
         id: 1,
@@ -14,7 +17,22 @@ const initialState = {
 };
 
 const todosReducer = (state = initialState, action) => {
+
+    Object.freeze(state);
+    const nextState = Object.assign({}, state);
+
     switch (action.type) {
+        case 'RECEIVE_TODOS':
+            action.todos.forEach((todo) => {
+                nextState[todo.id] = todo;
+            })
+
+            return nextState;
+
+        case 'RECEIVE_TODO': 
+            nextState[action.todo.id] = action.todo;
+            return nextState;
+
         default:
             return state;
     }
