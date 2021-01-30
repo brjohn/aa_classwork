e19cfb8760896f7310b084caaf879f0afcde8dba
Brynn <brynnreginajohnson@gmail.com>
2021-01-29 18:24:45 -0600

    Map and containery stuff

--- w10d4/Todos/frontend/components/app.jsx
+++ w10d4/Todos/frontend/components/app.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import TodoListContainer from './todos/todo_list_container';
 
 const App = () => {
     return (
         <div>
             <h1>To-Do's App</h1>
+            <TodoListContainer />
         </div>
     )
 }
